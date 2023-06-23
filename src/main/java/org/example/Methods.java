a338c0d7233cc7d2e5a8b2906d0063aea69b151d
TharaniU <tharaniudugampola2021@gmail.com>
2023-06-23 23:13:10 +0530

    Number count programme

---
+++ src/main/java/org/example/Methods.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Methods {
+    public static void main(String[] args) {
+        numberCount();
+
+    }
+
+    private static void numberCount() {
+        //counting 1 - 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(" "+i+" ");
+
+        }
+
+    }
+
+}
